78912a5b8fc25d9f786613dbb77a9dfbaa5f2a39
Nicholas Rucker <nick@ryans-network.local>
2019-09-14 12:35:45 -0400

    Implemented the algo for predicting one datapoint -Now I am going to work on predicting multiple datapoints -Also see 'Next Steps' from previous commit

--- nick_nn.py
+++ nick_nn.py
@@ -2,6 +2,8 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+from collections import Counter
 
 fileName = "FF74.txt"
 
@@ -32,14 +34,52 @@ for index, row in fishDS.iterrows():
 	if row[2] == 1:
 		color = 'k'
 	plt.scatter(row[0], row[1], color = color)
-plt.show()
+#plt.show()
 
+predict = [80, 7]
 
 def knn(train, test, k_neighbors):
-	for i in train:
-		for j in train[i]:
-			distance = sqrt((test[0] - j[0])**2 + (test[1] - j[1])**2)
-	return results
+	distances = []
+
+	# We are just going to loop through to find all the distances between the points
+	# Note this will only work for datasets with two features
+	# I'll find a work around for that in a little bit
+	for category in train:
+		for dataPoint in train[category]:
+			distance = math.sqrt((test[0] - dataPoint[0])**2 + (test[1] - dataPoint[1])**2)
+			distances.append([distance, category])
+
+	# We are throwing the decision logic into a loop in case there is a tie
+	while 1:
+		# We need to sort the distances because we care about the nearest neighbors
+		distances = sorted(distances)[:k_neighbors]
+
+		# And really we just need the group so lets turn the list of lists into a flat list
+		neighborList = [item for sublist in distances for item in sublist]
+		
+		cat0 = 0
+		cat1 = 0
+
+		for result in neighborList:
+			if result == 0:
+				cat0 += 1
+			else:
+				cat1 += 1
+
+		if cat1 > cat0:
+			return 1
+		elif cat0 > cat1:
+			return 0
+		else:
+			# If there is a tie and we can add a neighbor, we do
+			if len(train) - 3 > k_neighbors:
+				k_neighbors += 1
+			# If we cannot add a neighbor, we subtract one
+			else:
+				k_neighbors -= 1
+
+
+print(knn(fishMap, predict, 3))
 
 
 
