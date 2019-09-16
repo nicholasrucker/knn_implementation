c4f095bfbad9e484ea4dbe930d2324113bec3c60
Nicholas Rucker <nick@ryans-network.local>
2019-09-15 23:04:47 -0400

    Updated to be a user interactive program

--- nick_nn.py
+++ nick_nn.py
@@ -4,20 +4,8 @@ import matplotlib.pyplot as plt
 import math
 import itertools
 
-print("Enter a name for the input file:")
+print("\nEnter a name for the input file:")
 fileName = input()
-
-predict = []
-while True:
-	print("Enter a value for body length and dorsal fin length: ")
-	firstValue, secondValue = float(input()), float(input())
-	
-	if firstValue == 0 and secondValue == 0:
-		break
-
-	predict.append([firstValue, secondValue])
-
-fileName = "FF74.txt"
 
 # Here we are just converting the .txt to a .csv for easy retrieval of the data
 with open(fileName, 'r') as inputFile:
@@ -33,9 +21,17 @@ inputFile.close()
 fishDS = pd.read_csv(fileName.replace(".txt", ".csv"), header = None).dropna(inplace=False)
 fishDS = fishDS.sample(frac=1).reset_index(drop=True)
 
-# To normalize the data we need the max value in our two data columns
-maxBody = fishDS[0].max()
-maxDorsil = fishDS[1].max()
+# Lets normalize the data (Only columns 0 and 1 since 2 has our classification)
+cols_to_norm = [0,1]
+
+# First lets store the information to we can normalize data the user enters via command line later
+col0Mean = fishDS[0].mean()
+col1Mean = fishDS[1].mean()
+
+col0STD = fishDS[0].std()
+col1STD = fishDS[1].std()
+
+fishDS[cols_to_norm] = fishDS[cols_to_norm].apply(lambda x: (x - x.mean()) / x.std())
 
 # The map is going to have two keys: 0 and 1
 # The values will be a list of pairs which represent the datapoints.
@@ -54,26 +50,30 @@ trainingObs = fishDS.shape[0] // 5
 for index, row in fishDS.iterrows():
 	if trainingObs > 0:
 		if row[2] not in testingData.keys():
-			testingData[row[2]] = [[row[0] / maxBody, row[1] / maxDorsil]]
+			testingData[row[2]] = [[row[0], row[1]]]
 		else:
-			testingData[row[2]].append([row[0] / maxBody, row[1] / maxDorsil])
+			testingData[row[2]].append([row[0], row[1]])
 	else:
 		if row[2] not in trainingData.keys():
-			trainingData[row[2]] = [[row[0] / maxBody, row[1] / maxDorsil]]
+			trainingData[row[2]] = [[row[0], row[1]]]
 		else:
-			trainingData[row[2]].append([row[0] / maxBody, row[1] / maxDorsil])
+			trainingData[row[2]].append([row[0], row[1]])
 
 	trainingObs -= 1
 
+#####################################################
+# Un-comment this block if you would like to see a plot of the raw data
+# (The data that becomes the training data)
 	if row[2] not in fishMap.keys():
-		fishMap[row[2]] = [[row[0] / maxBody, row[1] / maxDorsil]]
+		fishMap[row[2]] = [[row[0], row[1]]]
 	else:
-		fishMap[row[2]].append([row[0] / maxBody, row[1] / maxDorsil])
-	color = 'r'
-	if row[2] == 1:
-		color = 'k'
-	plt.scatter(row[0], row[1], color = color)
-#############################################plt.show()
+		fishMap[row[2]].append([row[0], row[1]])
+# 	color = 'r'
+# 	if row[2] == 1:
+# 		color = 'k'
+# 	plt.scatter(row[0], row[1], color = color)
+# plt.show()
+#####################################################
 
 def knn(train, test, k_neighbors):
 	distances = []
@@ -106,9 +106,9 @@ def knn(train, test, k_neighbors):
 				cat1 += 1
 
 		if cat1 > cat0:
-			return 1
+			return '1'
 		elif cat0 > cat1:
-			return 0
+			return '0'
 		else:
 			# If there is a tie and we can add a neighbor, we do
 			if len(train) - 3 > k_neighbors:
@@ -117,21 +117,31 @@ def knn(train, test, k_neighbors):
 			else:
 				k_neighbors -= 1
 
-# for item in predict:
-# 	print(knn(fishMap, item, 3))
-totalRight = 0
-totalNumber = 0
-
 # Now lets see our accuracy with 1 to 10 neighbors
-for i in range(1,11):
-	for key, value in testingData.items():
-		for result in value:
-			predictedResult = knn(trainingData, result, i)
+# for i in range(1,22,2):
+# 	totalRight = 0
+# 	totalNumber = 0
+# 	falsePositives = 0
+# 	falseNegatives = 0
+# 	for key, value in testingData.items():
+# 		for result in value:
+# 			predictedResult = knn(trainingData, result, i)
 
-			if predictedResult == int(key):
-				totalRight += 1
-			totalNumber += 1
+# 			if predictedResult == int(key):
+# 				totalRight += 1
+# 			totalNumber += 1
 
-	print("Accuracy is for", i,"neighbors is:", totalRight/totalNumber)
+# 	print("K is:", i,"Total incorrect =", totalNumber - totalRight)
+# 	print(totalNumber)
 
+while 1:
+	predict = []
+	print("\nEnter a value for body length and dorsal fin length (press enter after each entry): ")
+	firstValue, secondValue = float(input()), float(input())
+	
+	if firstValue == 0 and secondValue == 0:
+		break
 
+	predict.append([(firstValue - col0Mean) / col0STD, (secondValue - col1Mean) / col1STD])
+
+	print("Predicted: TigerFish" + knn(fishMap, predict[0], 9))
